e415039eb3a0e10b9550f3ae57e5b8fb673285bc
czg <540674839@qq.com>
2018-09-12 15:58:22 +0800

    解决了cell中的Charts点击图表无法进入到cell的问题

--- stepbeats-ui01/clickView.swift
+++ stepbeats-ui01/clickView.swift
@@ -10,6 +10,7 @@ import UIKit
 //重写hittest方法来防止Charts捕获点击而进入不了cell
 class clickView: UIView {
 
+    //要是点击了cell中的view 直接返回这个view 不再对子view进行hittest
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         if(self.isHidden == false)  {
             if(self.point(inside: point, with: event)){
